f421a5816fa60ad1d80bf75ab0ed6bcc37a61b52
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-08-02 03:31:34 +0530

    change: remove Fatal from codederr

--- codederr.go
+++ codederr.go
@@ -3,37 +3,31 @@ package goerror
 type CodedError interface {
 	Error
 	Code() int
-	Fatal() bool
 }
 
-func NewCoded(code int, message string, isFatal bool) CodedError {
-	return &CodedErr{Err{&message, nil}, code, isFatal}
+func NewCoded(code int, message string) CodedError {
+	return &CodedErr{Err{&message, nil}, code}
 }
 
-func NewCodedEx(code int, message string, cause error, isFatal bool) CodedError {
-	return &CodedErr{Err{&message, cause}, code, isFatal}
+func NewCodedEx(code int, message string, cause error) CodedError {
+	return &CodedErr{Err{&message, cause}, code}
 }
 
-func CodedFrom(code int, err error, isFatal bool) CodedError {
+func CodedFrom(code int, err error) CodedError {
 	if err == nil {
 		return nil
 	}
 	if gerr, ok := err.(CodedError); ok && code == gerr.Code() {
 		return gerr
 	}
-	return &CodedErr{Err{nil, err}, code, isFatal}
+	return &CodedErr{Err{nil, err}, code}
 }
 
 type CodedErr struct {
 	Err
 	ErrCode int
-	IsFatal bool
 }
 
 func (h *CodedErr) Code() int {
 	return h.ErrCode
-}
-
-func (h *CodedErr) Fatal() bool {
-	return h.IsFatal
 }
