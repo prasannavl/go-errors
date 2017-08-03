968122b3b9bc62d7e4394c5dbe01261494bd263b
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-08-03 13:41:21 +0530

    test: CollectMsg tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,9 +2,10 @@ package utils_test
 
 import (
 	"errors"
+	"testing"
+
 	"github.com/prasannavl/goerror"
 	"github.com/prasannavl/goerror/utils"
-	"testing"
 )
 
 func TestIterator(t *testing.T) {
@@ -45,6 +46,42 @@ func TestIterator(t *testing.T) {
 	for i, exp := range expected {
 		if res[i] != exp {
 			t.Fatalf("result[%v]:%q doesn't match %q\nres: %#v\nexp: %#v", i, res[i], exp, res, expected)
+			break
+		}
+	}
+}
+
+func TestCollectMsg(t *testing.T) {
+	generalMsg := "some error"
+	innerMsg := "inner error"
+	wrapperMsg := "some wrapper error"
+
+	errs := []error{
+		goerror.New(generalMsg),
+		goerror.From(errors.New(innerMsg)),
+		goerror.NewEx(wrapperMsg, nil),
+		goerror.NewEx(wrapperMsg, errors.New(innerMsg)),
+	}
+
+	res := utils.CollectMsg(errs[0], nil)
+	if res[0] != generalMsg {
+		t.Fatalf("CollectMsg failed, expected %q got: %q", generalMsg, res)
+	}
+
+	res = utils.CollectMsgAll(errs, nil)
+
+	expected := []string{
+		generalMsg,
+		innerMsg,
+		wrapperMsg,
+		wrapperMsg,
+		innerMsg,
+	}
+
+	for i, exp := range expected {
+		if res[i] != exp {
+			t.Fatalf("result[%v]:%q doesn't match %q\nres: %#v\nexp: %#v", i, res[i], exp, res, expected)
+			break
 		}
 	}
 }
